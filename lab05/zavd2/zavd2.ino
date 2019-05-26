4d56ee0e04d98d4d1f1bbaed9d8ddd194842d666
Illya Chernyak <betmen19992@gmail.com>
2019-05-26 19:43:38 +0300

    add Lab05 zavd2 for arduino uno only!!!

---
+++ lab05/zavd2/zavd2.ino
@@ -0,0 +1,10 @@
+#include <avr/io.h>
+//Подключаем заголовочный файл, в котором объявлена
+//наша функция (и, если необходимо, еще что-то)
+#include "asmroutine.h"
+void setup() {
+  key_change();
+}
+void loop() {
+
+}
